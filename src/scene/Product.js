254922c04b59c6af399520f405826c753d515e91
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-08-07 15:47:48 +0900

    1. fix not important

--- src/scene/Product.js
+++ src/scene/Product.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+const ProductContainer = styled.div``;
 
 const Product = () => {
-  return <div>Product</div>;
+  return <ProductContainer>Product</ProductContainer>;
 };
 
 export default Product;
--- src/scene/Search.js
+++ src/scene/Search.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
+const SearchContainer = styled.div``;
+
 const Search = () => {
-  return <div>Search</div>;
+  return <SearchContainer>Search</SearchContainer>;
 };
 
 export default Search;
